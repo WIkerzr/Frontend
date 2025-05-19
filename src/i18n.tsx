05ee8fbbd5d6879bce6a87fc051923dff0cb9f1b
WIkerzr <i.zorrilla.grupo.campus@gmail.com>
2025-05-19 08:17:51 +0200

    i18 arreglo

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,24 +1,14 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import themeConfig from './theme.config';
+import es from '../public/locales/es.json';
+import eu from '../public/locales/eu.json';
 
-i18n
-    // load translation using http -> see /public/locales
-    // learn more: https://github.com/i18next/i18next-http-backend
-    .use(Backend)
-    // detect user language
-    // learn more: https://github.com/i18next/i18next-browser-languageDetector
-    .use(LanguageDetector)
-    // pass the i18n instance to react-i18next.
-    .use(initReactI18next)
-    // init i18next
-    // for all options read: https://www.i18next.com/overview/configuration-options
-    .init({
-        supportedLngs: ['es', 'eu'],
-        fallbackLng: themeConfig.locale || 'es',
-        debug: false,
-        load: 'languageOnly',
-    });
-export default i18n;
+i18n.use(initReactI18next).init({
+    resources: {
+        es: { translation: es },
+        eu: { translation: eu },
+    },
+    lng: 'es',
+    fallbackLng: 'es',
+    interpolation: { escapeValue: false },
+});
