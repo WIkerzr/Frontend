de26abd13614eb4c02593ef8e9a8cd6e9e174019
WIkerzr <i.zorrilla.grupo.campus@gmail.com>
2025-07-02 13:02:51 +0200

    Traducciones: Archivo adicional para errores

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,13 +2,15 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import es from '../public/locales/es.json';
 import eu from '../public/locales/eu.json';
+import errorEs from '../public/locales/errorEs.json';
+import errorEu from '../public/locales/errorEu.json';
 
 const idiomaGuardado = localStorage.getItem('idioma') || 'es';
 
 i18n.use(initReactI18next).init({
     resources: {
-        es: { translation: es },
-        eu: { translation: eu },
+        es: { translation: es, error: errorEs },
+        eu: { translation: eu, error: errorEu },
     },
     lng: idiomaGuardado,
     fallbackLng: 'es',
