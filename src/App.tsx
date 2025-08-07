bcbcd3e9ace2b9c2084f61090a2812032142059d
WIkerzr <i.zorrilla.grupo.campus@gmail.com>
2025-08-07 10:09:42 +0200

    Aviso modo desarrollo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from './store';
 import { toggleRTL, toggleTheme, toggleLocale, toggleMenu, toggleLayout, toggleAnimation, toggleNavbar, toggleSemidark } from './store/themeConfigSlice';
 import store from './store';
+import { ModoDev } from './components/Utils/gets/controlDev';
 
 function App({ children }: PropsWithChildren) {
     const themeConfig = useSelector((state: IRootState) => state.themeConfig);
@@ -25,7 +26,8 @@ function App({ children }: PropsWithChildren) {
                 themeConfig.rtlClass
             } main-section antialiased relative font-nunito text-sm font-normal`}
         >
-            {children}
+            {ModoDev && <div className="fixed top-0 left-0 right-0 z-50 bg-yellow-400 text-center text-black text-xs py-1 shadow-md">⚠️ MODO DESARROLLO</div>}
+            <div className={`${ModoDev ? 'pt-5' : ''}`}>{children}</div>
         </div>
     );
 }
